4a4a1b0962b932817d4f8d5e8720d466a603712f
Julio Menendez <juliomenendezdiaz@gmail.com>
2022-11-29 22:23:57 +0100

    hechos los objetos: Cell y Piece Loader. Iniciado el objeto grid

--- main.js
+++ main.js
@@ -21,53 +21,51 @@ class Piece {
     constructor (model, insert_point) {
         this.position = 0;
         this.anchor = insert_point;
-        if (model === 'purple') {
+        if (model === 'purplePiece') {
             this.points = [['0,-1', '1,0', '2,0'], ['1,0', '0,1', '0,2'], ['0,1', '-1,0', '-2,0'], ['-1,0', '0,-1','0,-2']];
-            this.class = 'purplePiece' 
+            this.class = model;
         }
-        else if (model === 'darkBlue') {
+        else if (model === 'darkBluePiece') {
             this.points = [['-1,0', '0,-1', '1,-1'], ['0,-1', '1,0', '1,1']];
-            this.class = 'darkBluePiece'
+            this.class = model;
         }
-        else if (model === 'yellow') {
+        else if (model === 'yellowPiece') {
             this.points = [['0,-1', '-1,0', '-2,0'], ['1,0', '0,-1', '0,-2'], ['0,-1', '1,-1', '1,-2'], ['-1,0', '0,1', '0,2']];
-            this.class = 'yellowPiece';
+            this.class = model;
         }
-        else if (model === 'orange') {
+        else if (model === 'orangePiece') {
             this.points = [['-1,0', '0,-1', '-1,-1'], ['0,1', '1,0', '1,-1']];
-            this.class = 'orangePiece';
+            this.class = model;
         }
-        else if (model === 'red') {
+        else if (model === 'redPiece') {
             this.points = [['0,-1', '0,-2', '0,-3'], ['1,0', '2,0', '3,0']];
-            this.class = 'redPiece';
+            this.class = model;
         }
-        else if (model === 'green') {
+        else if (model === 'greenPiece') {
             this.points = [['-1,0', '1,0', '0,-1'], ['0,-1', '1,0', '0,1'], ['-1,0', '1,0', '0,1'], ['0,-1', '-1,0', '0,1']];
-            this.class = 'greenPiece'
+            this.class = model;
         }
-        else if (model === 'blue') {
+        else if (model === 'bluePiece') {
             this.points = [['0,-1', '1,-1', '1,0']];
-            this.class = 'bluePiece';
+            this.class = model;
         }
     }
     //this method MODIFY the actual points.
-    //step: is the coordinates of the movement. examples: '1,0'(left), '0,1'(right) and '0,1'(down)
+    //step: is the coordinates of the movement. examples: '-1,0'(left), '1,0'(right) and '0,1'(down)
     //spin: indicates if we want to spin the piece. 'false': not spin and 'true' a 90 degress spin clockwise.
     //type: indicates if the new coordinates are real or only a data to check a future movement. 'true': the modifications are real. 'false': the object piece is unchanged.
     movePiece (step, spin, type) {
         const temp_anchor = sumTwoStrNums(this.anchor, step);
         let temp_position = this.position;
         if (spin === true) {
-            console.log(temp_position);
-            console.log(this.points[0]);
-            if (temp_position === this.points.length-1) {
+                if (temp_position === this.points.length-1) {
                 temp_position = 0;}
             else {
                 temp_position++}
             }
         const result = [temp_anchor];
         for (let i = 0; i<this.points[temp_position].length; i++) {
-            let temp_new_element = sumTwoStrNums(temp_anchor, this.points[temp_position][i]);
+            const temp_new_element = sumTwoStrNums(temp_anchor, this.points[temp_position][i]);
             result.push(temp_new_element);
         }
         if (type === true) {
@@ -81,11 +79,124 @@ class Piece {
 }
 
 
-a = new Piece ('blue', '1,1');
-console.log(a.movePiece('0,0', true, true));
-console.log(a.anchor);
-console.log(a.position);
+// a = new Piece ('blue', '1,1');
+// console.log(a.movePiece('0,0', true, true));
+// console.log(a.anchor);
+// console.log(a.position);
+
+
+class PieceLoader {
+    constructor() {
+        this.pieces = ['purplePiece', 'darkBluePiece', 'bluePiece', 'yellowPiece', 'greenPiece', 'redPiece', 'orangePiece'];
+        this.insert_point = '1,1';
+        this.piece = false;
+        this.RPG();
+
+    }
+    //Random Piece Generator//
+    RPG ()  {
+        const seed = Math.floor(Math.random()*this.pieces.length);
+        if (seed === 7) {
+            seed = 6;
+        }
+        this.piece = new Piece(this.pieces[seed], this.insert_point);
+    }
+    //Pop and return the piece and generate a new random piece// 
+    popPiece () {
+        const result = this.piece;
+        this.RPG();
+        return result;
+    }
+}
+
+// PL = new PieceLoader();
+// console.log(PL);
+// console.log(PL.piece);
+// PL.RPG();
+// console.log(PL.piece);
+// console.log(PL.popPiece());
+// console.log(PL.popPiece());
+// console.log(PL.popPiece());
+// console.log(PL.popPiece());
+// console.log(PL.popPiece());
+
+
+
+//Cell objects are meaning to fill a object grid//
+//this.cell keeps the piece in that cell, is equal a 'empty' if is unocupied//
+//this.div is the html tag of a div.
+
+class Cell {
+    constructor (x_pos, y_pos) {
+        //Logical data
+        this.cell = 'empty';
+        
+        
+        //Graphical data
+        this.div = document.createElement ('div');
+        this.div.setAttribute('class', 'celdaVacia');
+        this.div.style.left = `${this.x_pos}px`;
+        this.div.style.top = `${this.y_pos}px`;
+    }
+
+    setClass (new_class) {
+        this.div.setAttribute('class', new_class);
+    }
+
+    isOccupied () {
+        if (this.cell != 'empty') {
+            return false;
+        }
+        return true;
+    }
+
+    setPiece (piece) {
+        this.cell = piece;
+    }
+
+}
+
+class Grid {
+    constructor (width, height, insert_point) {
+        this.width = width;
+        this.height = height;
+        this.insert_point = insert_point;
+        this.active_piece = false;
+        this.left = false;
+        this.right = false;
 
 
 
 
+        //Graphical variables//
+        this.cell_obj = {};
+        this.div = document.createElement('div');
+        this.div.style.width = `${this.width}px`;
+        this.div.style.height = `${this.height}px`;
+        document.getElementById('root').appendChild(this.div);
+    }
+
+
+    getActualCells () {
+    
+        }
+
+        
+
+    checkMovement (futuro) {
+    
+    }
+
+    getFutureCells () {
+
+        }
+
+            
+    addPiece (new_piece) {
+
+        }
+
+
+
+
+    
