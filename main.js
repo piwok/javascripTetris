be30817a63d6af06ec548dcb937ced325596ee91
Julio Menendez <juliomenendezdiaz@gmail.com>
2023-02-14 12:06:11 +0100

    version funcional

--- main.js
+++ main.js
@@ -28,7 +28,8 @@ class Cell {
 
 
 class Grid {
-    constructor(width, invisible_height, visible_height, insert_point,css_grid, css_empty_cell) {
+    constructor(width, invisible_height, visible_height, insert_point,css_grid, css_empty_cell, tab_index) {
+        
         this.width = width;
         this.invisible_height = invisible_height;
         this.visible_height = visible_height;
@@ -43,12 +44,13 @@ class Grid {
         this.animation_speed = 4;
         this.div = document.createElement('div');
         this.div.setAttribute('class', this.css_grid);
-        this.div.tabIndex = 1;
         document.getElementById('root').appendChild(this.div);
+        this.div.tabIndex = tab_index;
+        this.div.focus({preventScroll: true});
         let new_cell = null;
         for (let i = 0; i < this.width; i++) {
             for (let j = 0; j < this.invisible_height + this.visible_height; j++) {
-                if (j < 5) {
+                if ((j < 5) && (this.css_grid != 'nextPieceGrid')) {
                     new_cell = new Cell('invisibleGridCell');
                     this.addCell(new_cell, `${i},${j}`);
                 }
@@ -76,6 +78,18 @@ class Grid {
     addPiece(new_piece) {
         this.active_piece = new_piece;
         this.active_piece.anchor = this.insert_point;
+    }
+    displayPiece(new_piece) {
+        for (let i = 0; i < 6; i++) {
+            for (let j = 0; j < 6; j++) {
+                this.cells_map.get(`${i},${j}`).setClass('nextPieceGridCell');
+            }
+        }
+        this.active_piece = new_piece;
+        this.active_piece.anchor = this.insert_point;
+        this.active_piece.getMoveStep('0,0', false).forEach((point) => {
+            this.cells_map.get(point).setClass(this.active_piece.model);
+        })
     }
     addController(new_controller) {
         this.active_controller = new_controller;
@@ -165,6 +179,7 @@ class Grid {
                 })
             }
             else {
+                this.checkAndClearLines();
                 this.addPiece(this.piece_loader.popPiece());
             }
             this.animation_counter = 0;
@@ -174,7 +189,30 @@ class Grid {
         }
     }
     checkAndClearLines() {
-        //to_do//
+        for (let j = 24; j > 4; j--) {
+            let flag = true;
+            for (let i = 0; i < 10; i++) {
+                if (this.cells_map.get(`${i},${j}`).piece === null) {flag = false;}
+            }
+            if (flag === true) {
+                for (let i = 0; i < 10; i++) {
+                    this.cells_map.get(`${i},${j}`).piece = null;
+                    this.cells_map.get(`${i},${j}`).setClass('whiteGridCell')
+                }
+                for (let jj = j; jj > 4; jj--) {
+                    for (let ii = 0; ii < 10; ii++) {
+                        this.cells_map.get(`${ii},${jj}`).piece = this.cells_map.get(`${ii},${jj-1}`).piece;
+                        if (this.cells_map.get(`${ii},${jj-1}`).piece === null) {
+                            this.cells_map.get(`${ii},${jj}`).setClass('emptyGridCell');
+                        }
+                        else {
+                            this.cells_map.get(`${ii},${jj}`).setClass(this.cells_map.get(`${ii},${jj-1}`).piece.model);
+                        }
+                    }
+                }
+                j = j + 1;
+            }   
+        }
     }
 }
 
@@ -248,9 +286,12 @@ class Piece {
     }
 }
 class PieceLoader {
-    constructor() {
+    constructor(main_grid, secondary_grid) {
+        this.main_grid = main_grid;
+        this.secondary_grid = secondary_grid;
         this.models = ['purplePiece', 'darkBluePiece', 'bluePiece', 'yellowPiece', 'greenPiece', 'redPiece', 'orangePiece'];
         this.piece = this.RPG();
+        this.main_grid.addPiece(this.popPiece());
     }
     RPG() {
         const seed = Math.floor(Math.random() * (this.models.length));
@@ -262,20 +303,20 @@ class PieceLoader {
     popPiece() {
         const piece = this.piece;
         this.piece = this.RPG();
+        this.secondary_grid.displayPiece(this.piece);
         return piece;
     }
 }
 class Controller {
-    constructor(grid, interval) {
-        this.grid = grid;
-        this.interval = interval;
+    constructor(main_grid) {
+        this.main_grid = main_grid;
         this.left = false;
         this.right = false;
         this.down = false;
         this.space = false;
         this.active = false;
         this.key_down_function = (event) => {
-            if (this.grid.active_piece != null) {
+            if (this.main_grid.active_piece != null) {
                 if (event.code === "ArrowLeft") {this.left = true;}
                 else if (event.code === "ArrowRight"){this.right = true;}
                 else if (event.code === "ArrowDown"){this.down = true;}
@@ -284,7 +325,7 @@ class Controller {
             document.querySelector('.mainGrid').addEventListener("keyup", this.key_up_function, {once:true});
         }
         this.key_up_function = (event) => {
-            if (this.grid.active_piece != null) {
+            if (this.main_grid.active_piece != null) {
                 if (event.code === "ArrowLeft") {this.left = false;} 
                 else if (event.code === "ArrowRight"){this.right = false;}
                 else if (event.code === "ArrowDown"){this.down = false;}
@@ -315,19 +356,17 @@ class Controller {
 document.addEventListener('DOMContentLoaded', main);
 //Main script//
 function main() {
-    let grid = new Grid(10, 5, 20, '4,4', 'mainGrid', 'emptyGridCell');
-    let piece_loader = new PieceLoader();
-    grid.addPiece(piece_loader.popPiece());
-    let main_controller = new Controller(grid);
-    grid.addController(main_controller);
-    grid.active_controller.setState(true);
-    grid.addPieceLoader(piece_loader);
+    let secondary_grid = new Grid(6, 0, 6, '3,3', 'nextPieceGrid', 'NextPieceGridCell', -1);
+    let main_grid = new Grid(10, 5, 20, '4,4', 'mainGrid', 'emptyGridCell', 1);
     
-    
-    
-
-
-// SetInterval script//
-    let main_grid_interval = setInterval(function () {grid.updateGrid();}, 100);
+    let piece_loader = new PieceLoader(main_grid, secondary_grid);
+    // secondary_grid.addPiece(piece_loader.popPiece());
+    let main_controller = new Controller(main_grid);
+    main_grid.addPieceLoader(piece_loader);
+    main_grid.addController(main_controller);
+    main_grid.addPiece(piece_loader.popPiece());
+    main_grid.active_controller.setState(true);
+ // SetInterval script//
+    let main_grid_interval = setInterval(() => {main_grid.updateGrid();}, 100);
 
 }
